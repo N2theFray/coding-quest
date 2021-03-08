f527e7ced6afc2595e8d957a71b466c5b4cd94f0
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-03-08 13:31:04 +0300

    added wipe functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var codeContentEl = document.querySelector("#codeContent")
-
+var questionHolderEL= document.querySelector("#questionHolder");
+var allContentEl = document.querySelector("#allContentHere");
+var incrementor = 0
 var codeQuestions = [
     "This is question 1",
     "This is question 2",
     "This is question 3"
 ]
-
 var codeAnswers = [
     "Q1 right answer",
     "Q1 one",
@@ -31,56 +32,67 @@ function timeGoesOn (){
 }
 
 function startChallenge (){
-
-    //remove original header and insert question
-    document.querySelector(".codeHeader").textContent = codeQuestions[0];
-    document.querySelector(".welcomeContent").textContent = "";
-
-    //remove quiz Start button
+    //remove quiz Start button and paragraph
     var removeStart = document.querySelector(".quizBtn");
     removeStart.remove();
-    debugger;
-    //div to hold buttons
-    var buttonHolder = document.createElement("div");
-    buttonHolder.className = "choiceBtns";
 
-    // loop to add three choices
-    for (var i = 0; i <= 2; i++){ 
-        //add buttons that contain answers
-    var answerBtn = document.createElement("button");
-    answerBtn.className = "answers";
-    answerBtn.textContent = codeAnswers[i];
-
-     //add buttons to button holder div
-     buttonHolder.appendChild(answerBtn);
-}
-
+    var removeP = document.querySelector(".welcomeContent");
+    removeP.remove();
+    
+    var removeCodeContent = document.querySelector("#codeContent");
+    removeCodeContent.remove();
+    //Start questions
+    questionBuilder();
    
-
-    //add button div to section
-    codeContentEl.appendChild(buttonHolder);
-    questionBuilder;
 }   
+function questionReset(){
+    var redButton = document.querySelector("#questionHolder")
+    redButton.remove();
 
+    questionBuilder();
+}
+//
 function questionBuilder () {
+
+    //create div to hold questions
+    var questionHolder = document.createElement("div");
+    questionHolder.id = "questionHolder"
+    
+    
+    //create question H1
+     var questionH1 = document.createElement("h1");
+     questionH1.className = "questionGoesHere";
+     questionH1.textContent = codeQuestions[incrementor];
+
+     questionHolder.appendChild(questionH1);
      //div to hold buttons
      var buttonHolder = document.createElement("div");
      buttonHolder.className = "choiceBtns";
+     buttonHolder.id= "nextQuestion";
  
-     //add buttons that contain answers
-     var answerBtn = document.createElement("button");
-     answerBtn.className = "answers";
-     answerBtn.textContent = codeAnswers[0];
+         // loop to add three choices
+         for (var i = 0; i <= 2; i++){ 
+             //add buttons that contain answers
+         var answerBtn = document.createElement("button");
+         answerBtn.className = "answers";
+         answerBtn.textContent = codeAnswers[i];
  
-     //add buttons to button holder div
-     buttonHolder.appendChild(answerBtn);
+         //add buttons to button holder div
+         buttonHolder.appendChild(answerBtn);
+         }
  
      //add button div to section
-     codeContentEl.appendChild(buttonHolder);
+     questionHolder.appendChild(buttonHolder);
 
-     startChallenge()
+     //add all content to body
+     allContentEl.appendChild(questionHolder);
+    incrementor ++;
+    // debugger;
+    
+    questionHolder.addEventListener("click", questionReset);
 }
 
+// questionHolderEL.addEventListener("click", questionBuilder);
 codeContentEl.addEventListener("click",timeGoesOn);
 codeContentEl.addEventListener("click", startChallenge);
 console.log(codeQuestions[0]);

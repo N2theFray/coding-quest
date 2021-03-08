cfe79afc00f8e94748c72dabf9f46622632bcb7f
Scuba Steve <78673754+blacksteven1@users.noreply.github.com>
2021-03-08 11:52:45 +0300

    created for loop to put down three buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ var codeAnswers = [
     "Q3 three"
 ]
 
+// timer function
 function timeGoesOn (){
     var timeLeft = 100;
     var downloadTimer = setInterval(function(){
@@ -29,5 +30,57 @@ function timeGoesOn (){
     },1000);
 }
 
+function startChallenge (){
+
+    //remove original header and insert question
+    document.querySelector(".codeHeader").textContent = codeQuestions[0];
+    document.querySelector(".welcomeContent").textContent = "";
+
+    //remove quiz Start button
+    var removeStart = document.querySelector(".quizBtn");
+    removeStart.remove();
+    debugger;
+    //div to hold buttons
+    var buttonHolder = document.createElement("div");
+    buttonHolder.className = "choiceBtns";
+
+    // loop to add three choices
+    for (var i = 0; i <= 2; i++){ 
+        //add buttons that contain answers
+    var answerBtn = document.createElement("button");
+    answerBtn.className = "answers";
+    answerBtn.textContent = codeAnswers[i];
+
+     //add buttons to button holder div
+     buttonHolder.appendChild(answerBtn);
+}
+
+   
+
+    //add button div to section
+    codeContentEl.appendChild(buttonHolder);
+    questionBuilder;
+}   
+
+function questionBuilder () {
+     //div to hold buttons
+     var buttonHolder = document.createElement("div");
+     buttonHolder.className = "choiceBtns";
+ 
+     //add buttons that contain answers
+     var answerBtn = document.createElement("button");
+     answerBtn.className = "answers";
+     answerBtn.textContent = codeAnswers[0];
+ 
+     //add buttons to button holder div
+     buttonHolder.appendChild(answerBtn);
+ 
+     //add button div to section
+     codeContentEl.appendChild(buttonHolder);
+
+     startChallenge()
+}
+
 codeContentEl.addEventListener("click",timeGoesOn);
+codeContentEl.addEventListener("click", startChallenge);
 console.log(codeQuestions[0]);
